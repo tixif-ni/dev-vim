955714ca98a47b6848ab4f47ececf31e402b8391
Ernesto Chavez <ernesto.chavez@taxfyle.com>
2023-10-26 19:46:46 -0600

    chore: code cleanup

--- lua/telescope/_extensions/git_signs.lua
+++ lua/telescope/_extensions/git_signs.lua
@@ -1,11 +1,14 @@
-local utils = require("telescope.utils")
+local telescope_utils = require("telescope.utils")
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
 local entry_display = require("telescope.pickers.entry_display")
+local cache = require("gitsigns.cache").cache
+local config = require("gitsigns.config").config
 
 local entry_maker = function(entry)
-    local config = require("gitsigns.config").config
+    local hunk = entry.hunk
+    local text = string.format("Lines %d-%d", hunk.added.start, hunk.vend)
 
     local displayer = entry_display.create({
         separator = "",
@@ -16,44 +19,39 @@ local entry_maker = function(entry)
     })
 
     local make_display = function()
-        local sign = config.signs[entry.type]
+        local sign = config.signs[hunk.type]
 
         return displayer({
             {
                 sign.text,
                 sign.hl,
             },
-            entry.text,
+            text,
         })
     end
 
     return {
         value = entry,
-        ordinal = entry.text,
+        ordinal = text,
         display = make_display,
-        lnum = entry.lnum,
+        lnum = hunk.added.start,
         filename = entry.filename,
     }
 end
 
 local generate_new_finder = function()
-    local cache = require("gitsigns.cache").cache
-
     local current_buf = vim.api.nvim_get_current_buf()
     local results = {}
 
-    local buff_cache = cache[current_buf]
+    local bcache = cache[current_buf]
 
-    if buff_cache then
-        local hunks = buff_cache.hunks
+    if bcache then
+        local hunks = bcache.hunks
 
-        for i, hunk in ipairs(hunks) do
+        for _, hunk in ipairs(hunks) do
             table.insert(results, {
-                bufnr = current_buf,
-                filename = buff_cache.file,
-                lnum = hunk.added.start,
-                type = hunk.type,
-                text = string.format("Lines %d-%d (%d/%d)", hunk.added.start, hunk.vend, i, #hunks),
+                filename = bcache.file,
+                hunk = hunk,
             })
         end
     end
@@ -77,16 +75,11 @@ local git_signs = function(opts)
                 prompt_title = "Git Hunks",
                 finder = finder,
                 sorter = conf.generic_sorter(opts),
-                -- TODO: Would it be better to use git diff previewer?
                 previewer = conf.grep_previewer(opts),
-                attach_mappings = function(prompt_bufnr, map)
-                    -- TODO: Add mappings to stage | reset hunks
-                    return true
-                end,
             })
             :find()
     else
-        utils.notify("git_signs", {
+        telescope_utils.notify("git_signs", {
             msg = "No git hunks found",
             level = "INFO",
         })
